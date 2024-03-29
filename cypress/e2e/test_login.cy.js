faa3d0475b2b4ae94a09473752be6325ca1958ae
koong pok <126952959+Nuzulius@users.noreply.github.com>
2024-03-29 11:42:28 +0700

    initial commit

---
+++ cypress/e2e/test_login.cy.js
@@ -0,0 +1,6 @@
+describe('Launch Application', () => {
+  it('Launch annotation tool', () => {
+    // cy.visit('https://annotation.fata-organa.com/auth/login')
+    cy.visit('https://automlvmjptest01.japaneast.cloudapp.azure.com/annotation/auth/login')
+  })
+})
