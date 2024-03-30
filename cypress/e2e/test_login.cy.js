000309a0990904dd1b6c2bcc0fd6c763de841ccd
koong pok <126952959+Nuzulius@users.noreply.github.com>
2024-03-30 15:19:08 +0700

    add - login scenario

--- cypress/e2e/test_login.cy.js
+++ cypress/e2e/test_login.cy.js
@@ -1,43 +1,83 @@
 import loginLocators from "../fixtures/locators/login_locator.json";
+import loginFixtures from "../fixtures/data/login_fixtures.json";
 
 describe("Login Scenario", () => {
   it("Launch Annotation Tool", () => {
     cy.launch_app();
-    // cy.visit(
-    //   "https://automlvmjptest01.japaneast.cloudapp.azure.com/annotation/auth/login"
-    // );
-    // cy.visit('https://annotation.fata-organa.com/auth/login')
   });
 
   it("Change language", () => {
     cy.launch_app();
 
-    // Define language elements
-    const japaneseElements = ["ユーザー名", "パスワード"];
-    const englishElements = ["Username", "Password"];
+    // Define elements
+    const jp = ["ユーザー名", "パスワード"];
+    const en = ["Username", "Password"];
 
-    // Click on the Japanese flag and verify Japanese elements
+    // process
     cy.get(loginLocators.jp_flag).click();
-    japaneseElements.forEach((element) => cy.contains(element).should("exist"));
+    jp.forEach((element) => cy.contains(element).should("exist"));
 
-    // Click on the US flag and verify English elements
     cy.get(loginLocators.us_flag).click();
-    englishElements.forEach((element) => cy.contains(element).should("exist"));
+    en.forEach((element) => cy.contains(element).should("exist"));
   });
 
-  // it("Login process - invalid", () => {
-  //   cy.fixture("example.json").then((exampleData) => {
-  //     const username = exampleData.name;
-  //     const password = exampleData.name;
+  it("Login process - invalid", () => {
+    cy.launch_app();
 
-  //     cy.get("input[type='text']")
-  //       .type(username)
-  //       .should("have.value", username)
-  //       .wait(1000);
-  //     cy.get("input[type='password']").type(password);
-  //   });
-  //   cy.get("//button[@type='submit']").click();
-  // });
+    // Define elements
+    const validation = ["ログアウト", "logout"];
 
-  // it("Login process - valid", () => {});
+    // process
+    loginFixtures.login_invalid.forEach((credentials) => {
+      if (credentials.username === "" || credentials.password === "") {
+        return;
+      }
+
+      cy.get(loginLocators.username_input).clear().type(credentials.username);
+      cy.get(loginLocators.password_input).clear().type(credentials.password);
+      cy.get(loginLocators.login_button).click();
+
+      validation.forEach((element) => {
+        cy.contains(element).should("not.exist");
+      });
+    });
+  });
+
+  it("Login process - valid", () => {
+    cy.launch_app();
+
+    // Define elements
+    const languages = {
+      jp: {
+        flag: loginLocators.jp_flag,
+        elements: ["ユーザー名", "パスワード"],
+      },
+      en: { flag: loginLocators.us_flag, elements: ["Username", "Password"] },
+    };
+
+    const performLogin = (language) => {
+      const { flag, elements } = languages[language];
+
+      cy.get(flag).click();
+      elements.forEach((element) => cy.contains(element).should("exist"));
+
+      loginFixtures.login_valid.forEach((credentials) => {
+        if (credentials.username !== "" && credentials.password !== "") {
+          cy.get(loginLocators.username_input)
+            .clear()
+            .type(credentials.username);
+          cy.get(loginLocators.password_input)
+            .clear()
+            .type(credentials.password);
+          cy.get(loginLocators.login_button).click();
+          cy.wait(2000);
+          cy.get(loginLocators.logout_button).click();
+        }
+      });
+    };
+
+    Object.keys(languages).forEach((language) => performLogin(language));
+  });
+
+  //
 });
